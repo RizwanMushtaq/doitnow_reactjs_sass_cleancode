bb820c5ac8d514c01eedd618fe140e495cf3d9a9
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-10-02 09:02:59 +0200

    changing style of Dialog

--- src/components/AppPageComponents/AddToDoItemDialog.js
+++ src/components/AppPageComponents/AddToDoItemDialog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import Style  from './AddToDoItemDialog.module.scss'
 
 export default function AddToDoItemDialog({
@@ -9,12 +9,14 @@ export default function AddToDoItemDialog({
     
     let selectedDate = selectedDay + '.' + selectedMonth + '.' + selectedYear
     
+    const autoFocus = useCallback(el => el ? el.focus() : null, [])
+
     return (
         <div className={Style.container}>
             <div></div>
             <div className={Style.dialogBox}>
                 <div className={Style.dateLabel}>{selectedDate}</div>
-                <textarea className={Style.textArea} rows="3" autofocus placeholder="Write your Todo Item.."/>
+                <textarea className={Style.textArea} rows="3" ref={autoFocus} placeholder="Write your Todo Item.."/>
                 <div className={Style.buttonContainer}>
                     <button onClick={handleCancelButtonClick}>Cancel</button>
                     <button>Save</button>
